7a26175a419b8c2bc357653eb04a1d2c8819e83a
Pradeep Kumar <pradeep4all2003@gmail.com>
2026-08-18 23:21:09 +0530

    Update main.tf

--- Environment/Prod/main.tf
+++ Environment/Prod/main.tf
@@ -1,9 +1,9 @@
 module "resource_group" {
-  source          = "../../modules/azurerm_resource_group"
+  source          = "../../module/azurerm_resource_group"
   resource_groups = var.rgs
 }
 
 # module "storage_account" {
-#   source           = "../../modules/azurerm_storage_account"
+#   source           = "../../module/azurerm_storage_account"
 #   storage_accounts = var.storage_accounts
 # }
